0ee04556b341d97a15fb761cc023b2b7abcf5871
jacklord1 <obdwase@gmail.com>
2026-04-07 18:51:54 +0100

    chore(config): whitelist S3 and Contentful image hostnames

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "ccfc-gall.s3.eu-central-1.amazonaws.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.contentful.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.ctfassets.net",
+      }
+    ],
+  },
 };
 
 export default nextConfig;
